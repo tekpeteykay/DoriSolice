7bebc2ad37832ad68bc32e2b07eecf6e9aae94ec
tekpeteykay <tekpeteykay@gmail.com>
2026-09-19 17:26:09 +0100

    commit js file

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [],
+    remotePatterns: [
+      { protocol: "https", hostname: "*.supabase.co", pathname: "/storage/v1/object/public/**" },
+    ],
   },
 };
 
